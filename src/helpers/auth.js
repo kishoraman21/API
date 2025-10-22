45e06421e1208344761dd48dd004586fcaf61f84
kishoraman21 <kishoraman2121@gmail.com>
2025-10-22 21:47:26 +0530

    fixed credentials signup

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/userModel";
 import { connect } from "../dbConfig/db";
+import crypto from "crypto";
 
 export async function signUpUser(username, email, password) {
   await connect();
@@ -10,12 +11,21 @@ export async function signUpUser(username, email, password) {
     throw new Error("User already exists!");
   }
 
+  // const hashedPassword = await bcrypt.hash(password, 10);
+
+  // password = hashedPassword;
+
+  const newApikey = crypto.randomBytes(40).toString("hex");
+
+  
   
   const user = await User.create({
     username,
     email,
     password, 
     provider: 'credentials',
+    apikey: newApikey,
+
   });
 
   return { email: user.email, apikey: user.apikey };
